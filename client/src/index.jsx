ff5079b7ef220558f8dc839346b7f9830efb115f
Jonathan Ling <electric.pen@gmail.com>
2017-09-28 12:11:53 -0700

    Added skeleton structure for app and working package.json

---
+++ client/src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './login.js';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      rider: false,
+      driver: false,
+      startPoint: '',
+      endPoint: '',
+      departureTime: '00:00:00',
+      car: '',
+      seats: 0,
+      schedule: [] // schedule will contain rideId#'s that refer to entries in the rides join table
+    }
+  }
+  render() {
+    return (
+      <Login />
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById(app));
---
+++ client/src/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = (props) => (
+    <div>Login</div>
+    <div>Register</div>
+)
+
+
+export default Login;

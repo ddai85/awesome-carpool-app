89408463964b3ba4ffdf94acd7d7a581ebd2fb8b
Brian Pak <brianpak27@gmail.com>
2017-09-28 12:23:06 -0700

    database schema complete

---
+++ database/schema.sql
@@ -0,0 +1,45 @@
+
+DROP DATABASE IF EXISTS pools;
+
+CREATE DATABASE pools;
+
+USE pools;
+
+CREATE TABLE driver (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  car VARCHAR(50) NOT NULL,
+  license VARCHAR(50) NOT NULL,
+  seats INT NOT NULL,
+  home VARCHAR(100) NOT NULL,
+  work VARCHAR(100) NOT NULL,
+  departureTime TIME NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rider (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rides (
+  id INT NOT NULL AUTO_INCREMENT,
+  driverID INT NOT NULL,
+  driverName VARCHAR(50) NOT NULL,
+  rider1 INT,
+  rider2 INT,
+  rider3 INT,
+  rider4 INT,
+  rider5 INT,
+  rider6 INT,
+  departureDate DATE NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(driverID) REFERENCES driver(id),
+  FOREIGN KEY(rider1) REFERENCES rider(id),
+  FOREIGN KEY(rider2) REFERENCES rider(id),
+  FOREIGN KEY(rider3) REFERENCES rider(id),
+  FOREIGN KEY(rider4) REFERENCES rider(id),
+  FOREIGN KEY(rider5) REFERENCES rider(id),
+  FOREIGN KEY(rider6) REFERENCES rider(id)
+)

cf9dfc82b2c0ba86835f2519a0c94c9b2ae6fb25
Brian Pak <brianpak27@gmail.com>
2017-10-02 16:56:58 -0700

    add some html dropdown menu

--- client/src/rider.jsx
+++ client/src/rider.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 const RiderPage = (props) => (
   <div>Rider Page</div>
+  <select Time>
+    <option value="startTime">7:00 am</option>
+    <option value="starTime">7:30 am</option>
+    <option value="startTime">8:00 am</option>
+    <option value="startTime">8:30 am</option>
+  </select>
+
+  <select StartPoint>
+    <option value="startPoint">Oakland</option>
+    <option value="startPoint">San Jose</option>
+  </select>
+
+  <select Endpoint>
+    <option value="endPoint">Downtown</option>
+    <option value="endPoint">Hack Reactor</option>
+  </select>
+
 );
 
 export default RiderPage;

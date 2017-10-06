b77fa73c7a8efb45b7cf2970c499943020ef4fb1
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-06 14:16:38 -0700

    Complete database method to insert driver schedule into the rides table

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
       driver: false,
       home: '',
       work: '',
-      departureTime: '',
+      departureTime: '08:00:00',
       car: '',
       seats: 0,
       schedule: [] // schedule will contain rideId#'s that refer to entries in the rides join table
@@ -32,6 +32,7 @@ class App extends React.Component {
     this.handleEndPoint = this.handleEndPoint.bind(this);
     this.checkUser = this.checkUser.bind(this);
     this.setUserPage = this.setUserPage.bind(this);
+    this.postRideSchedule = this.postRideSchedule.bind(this);
   }
 
 
@@ -95,7 +96,6 @@ class App extends React.Component {
   }
 
   handleTime(e) {
-
 
   }
 
@@ -162,8 +162,9 @@ class App extends React.Component {
   
   //post request from driver page when date and time is submitted
   //post to '/driver' endpoint and insert into rides database
-  postRideSchedule(driver, date, time) {
-    var rideSchedule = {driver: driver, date: date, time: time};
+  postRideSchedule(driver, date) {
+    console.log('postRideSchedule!!!!');
+    var rideSchedule = {username: driver, departureDate: date};
     $.ajax({
       method: 'POST',
       url: '/driver', 
@@ -187,9 +188,9 @@ class App extends React.Component {
         {this.state.page === 'login'
           ? <div></div>
           : this.state.page === 'registration'
-            ? <RegistrationPage saveDriver={this.saveDriver} saveRider={this.saveRider} username={this.state.username} setUserPage={this.setUserPage}/>
+            ? <RegistrationPage saveDriver={this.saveDriver} saveRider={this.saveRider} username={this.state.username} setUserPage={this.setUserPage} />
             : this.state.page === 'driver'
-              ? <DriverPage driver={this.state.driver} getRideSchedule={this.getRideSchedule} schedule={this.state.schedule} />
+              ? <DriverPage username={this.state.username} departureTime={this.state.departureTime} getRideSchedule={this.getRideSchedule} postRideSchedule={this.postRideSchedule} schedule={this.state.schedule} />
               : <RiderPage />
        }
       </div>

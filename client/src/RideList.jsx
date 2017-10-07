d8692c1fd5411cdf4b7bf53cefc2dca94a984e62
Jonathan Ling <electric.pen@gmail.com>
2017-10-07 16:58:49 -0700

    Debug display of list items in search results on rider page

--- client/src/RideList.jsx
+++ client/src/RideList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const RideList = (props) => (
   <div className="container-fluid">
-    <div>The following rides are available on your selected date:</div>
+    <div className="text">The following rides are available on your selected date:</div>
     {props.rides.map((rideObj, index) => {
       // check if ride is full
       if (rideObj[`rider${rideObj.seats}`] === null) {
-        return <p>{`${index + 1}: ${rideObj.username}'s ${rideObj.car}`}</p>
+        return <p key={index.toString()} className="text">{`${index + 1}: ${rideObj.username}'s ${rideObj.car}`}</p>
       }
     }) }
   </div>

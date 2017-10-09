7f027b3e15f4f99fc908e1f80547d93f82707d2e
Jonathan Ling <electric.pen@gmail.com>
2017-10-09 14:18:49 -0700

    Wrote basic documentation for client-side file structure

--- spec/client.js
+++
@@ -1,5 +0,0 @@
-var login = require('../client/src/login');
-var reg = require('../client/src/registrationPage');
-var rider = require('../client/src/rider');
-var driver = require('../client/src/driverPage');
-var index = require('../client/src/index');
